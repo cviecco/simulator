5c35633c4af42255219e7d4e980c68350ccb2aed
Camilo Hernan Viecco <cviecco@meta.com>
2026-02-12 14:27:42 -0800

    move-files part1

---
+++ include/print.go
@@ -0,0 +1,17 @@
+package include
+
+/*
+ This file is just to try to force the use fo cgo within the package
+ code coming primarily from: https://go.dev/blog/cgo
+*/
+
+// #include <stdio.h>
+// #include <stdlib.h>
+import "C"
+import "unsafe"
+
+func Print(s string) {
+	cs := C.CString(s)
+	defer C.free(unsafe.Pointer(cs))
+	C.fputs(cs, (*C.FILE)(C.stdout))
+}
