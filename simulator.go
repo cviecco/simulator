08a40a20cd7517a1aa98323bdd6317a6a19cf6d0
Camilo Viecco <cviecco@meta.com>
2026-02-13 09:49:20 -0800

    Moved files

--- simulator.go
+++ simulator.go
@@ -25,7 +25,7 @@ import (
 	"sync"
 
 	"github.com/cviecco/simulator/internal"
-	_ "github.com/cviecco/simulator/internal/Ossl"
+	//_ "github.com/cviecco/simulator/internal/Ossl"
 
 	//_ "github.com/cviecco/simulator/internal/src"
 	"github.com/google/go-tpm/legacy/tpm2"
